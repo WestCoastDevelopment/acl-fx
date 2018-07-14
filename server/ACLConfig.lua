d786ed4c7de4c423464a100747208bc4e55f3c2a
WestCoastWIll <41215321+WestCoastWill@users.noreply.github.com>
2018-07-13 21:32:42 -0400

    Add files via upload
    Official files added.

---
+++ server/ACLConfig.lua
@@ -0,0 +1,24 @@
+-- Check the ping of the client attempting to connect. Set to 0 to disable this check
+ACL.maxPing = 120
+
+-- Set to false to disable whitelisting
+ACL.enableWhitelist = false
+
+-- Set to true to enable the /kick and /playerlist commands
+ACL.enablePlayerManagement = true
+
+-- Set to false to disable the check whether identities really belong to connecting player (causes "please try again" message)
+ACL.useIdentBugWorkaround = true
+
+ACL.whitelist = {
+}
+
+-- Currently mods and admins are indistinguishable
+ACL.mods = {
+}
+
+ACL.admins = {
+}
+
+ACL.banlist = {
+}
